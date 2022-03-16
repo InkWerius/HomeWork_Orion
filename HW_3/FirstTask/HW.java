77473846f993a21e66bf6b2ddaafb1743ca77e49
A|K <97691212+InkWerius@users.noreply.github.com>
2022-03-16 20:23:54 +0300

    Add files via upload

---
+++ HW_3/FirstTask/HW.java
@@ -0,0 +1,57 @@
+package Learning;
+
+//Задание 1. Цифровая персонализация
+//        - Создать класс Person с двумя полями типа String: firstName, lastName
+//        - создать один публичный конструктор с аргументами firstName и lastName.
+//        В конструкторе инициализировать поля класса значениями из аргументов
+//
+//        - Для каждого поля определить только getter'ы (без setter'ов) - getFirstName, getLastName , который возвращают соответствующее поле
+//        - Создать метод toString() который возвращает человека в формате "Фамилия, Имя" в методе main
+//        - Создать массив типа String на 5 элементов - туда записать какие-нибудь реальные имена.
+//        - Создать массив типа String на 6 элементов - туда записать какие-нибудь реальные фамилии
+//        - Создать массив типа Person на 50 элементов - туда записать людей со случайными фамилиями из массивов выше
+//        - Вывести всех людей из массива в формате "Человек №{индекс в массиве} - Фамилия, Имя"
+
+
+import java.util.Random;
+
+class Person {
+    private String firstName, lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return lastName + "," + firstName;
+    }
+}
+
+public class HW{
+    public static void main(String[] args) {
+        String[] names = {"Vincent", "Melanie", "Johnny", "Tim", "Elizabeth"};
+        String[] last_names= {"Pride", "Watson", "Depp", "Trump","Pitt", "Hascoat"};
+        Person[] people = new Person[50];
+        int rand_name;
+        int rand_lastname;
+        for (int i = 0; i < people.length; i++) {
+            rand_name = new Random().nextInt(5);
+            rand_lastname = new Random().nextInt(6);
+            people[i] = new Person(names[rand_name], last_names[rand_lastname]);
+        }
+        for (int i = 0; i < people.length; i++) {
+//            "Человек №{индекс в массиве} - Фамилия, Имя"
+            System.out.println("Человек №"+ (i+1) + " - " + people[i].toString());
+        }
+    }
+}
---
+++ HW_3/SecondTask/HW.java
@@ -0,0 +1,60 @@
+package Learning;
+
+//Задание 2. Случайности - не случайны
+//        Создать класс SequentialRandom с одним полем типа int
+//        Значение поле можно только прочитать, но не изменить (используйте модифиактор private
+//на поле и создайте только getter)
+//        Значение этого int вычисляется по следующим правилом:
+//        - Значение для первого созданного объекта- случайное
+//        - значение для каждого следующего на 1 больше, чем предыдущее
+//        - У класса есть статический метод resetRandom(), который делает значение следующего
+//        созданного объекта - случайным.
+//        Например new SequentialRandom() // содержит 5 (случайное)
+//        // new SequentialRandom() // содержит 6 (на 1 больше)
+//        // new SequentialRandom() // содержит 7 (на 1 больше)
+//        SequentialRandom.resetRandom()
+//        // new SequentialRandom() // содержит 42 (случайное
+//        // new SequentialRandom() // содержит 43) (на 1 больше)
+//        Подсказка: Используйте статическую переменную в классе, чтобы хранить значение предыдущего объекта.
+
+import java.util.Random;
+
+class SequentialRandom {
+    private int digit = container;
+    static int container = new Random().nextInt(100);
+
+    public SequentialRandom() {
+        container++;
+    }
+
+    public int getDigit() {
+        return digit;
+    }
+
+    static void resetRandom() {
+        container = new Random().nextInt(100);
+    }
+
+}
+
+public class HW {
+    public static void main(String[] args) {
+//TEST
+        SequentialRandom a = new SequentialRandom();
+        SequentialRandom a1 = new SequentialRandom();
+        SequentialRandom a2 = new SequentialRandom();
+        System.out.println(a.getDigit());
+        System.out.println(a1.getDigit());
+        System.out.println(a2.getDigit());
+
+        SequentialRandom.resetRandom();
+
+        SequentialRandom b = new SequentialRandom();
+        SequentialRandom b1 = new SequentialRandom();
+        SequentialRandom b2 = new SequentialRandom();
+        System.out.println(b.getDigit());
+        System.out.println(b1.getDigit());
+        System.out.println(b2.getDigit());
+
+    }
+}
